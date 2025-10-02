651ec67afe5ce86710efcfe225bc41f08e66391a
Dominic-SR <dominicseduraja3@gmail.com>
2025-10-02 23:44:00 +0530

    tailwind added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),],
   server: {
     host: true,   // 👈 important
     port: 5173,   // you can also change port here if you want
